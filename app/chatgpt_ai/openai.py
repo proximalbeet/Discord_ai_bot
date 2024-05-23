941e47dcfdc8667d7f1b602272ab294685dc46b7
proximalbeet <nake73a@gmail.com>
2024-05-23 17:59:23 -0400

    script now runs

--- app/chatgpt_ai/openai.py
+++ app/chatgpt_ai/openai.py
@@ -8,12 +8,12 @@ openai.api_key = os.getenv('CHATGPT_API_KEY')
 
 def chatgpt_response(prompt):
     response = openai.Completion.create(
-        model="text-davinci-003",
+        model="gpt-3.5-turbo-instruct",
         prompt=prompt,
         temperature=1,
         max_tokens=100
     )
-    response_dict = response.get("choice")
+    response_dict = response.get("choices")
     if response_dict and len(response_dict) > 0:
         prompt_response = response_dict[0] ["text"]
     return prompt_response
